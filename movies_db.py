bf6d5b7b655d834c525530d740f0d3dced363a8c
Jordan Howard <jordanhoward1991@gmail.com>
2022-07-07 15:35:32 -0600

    fixes upstream issues

---
+++ movies_db.py
@@ -0,0 +1,18 @@
+from flask import request, Flask, jsonify, Response
+from flask_marshmallow import Marshmallow
+from sqlalchemy.dialects.postgresql import UUID
+
+
+app = Flask(__name__)
+
+
+
+def create_all():
+  pass
+
+
+
+
+
+if __name__ == ('__main__'):
+  app.run()
