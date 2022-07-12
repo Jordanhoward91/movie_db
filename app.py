765fe747a0e38a3a91b89322e1dfbe3be3a42322
Jordan Howard <jordanhoward1991@gmail.com>
2022-07-12 17:03:17 -0600

    adds default movie for database

--- app.py
+++ app.py
@@ -89,6 +89,39 @@ movies_schema = MoviesSchema(many=True)
 def create_all():
     db.create_all()
 
+    print("Querying for Movie...")
+    movie_data = db.session.query(Movies).filter(Movies.movie_name == "Top Gun").first()
+    if movie_data == None:
+        print("Movie not found. Creating Top Gun...")
+        actor_data = db.session.query(Actors).filter(Actors.actor_first_name == "Tom").first()
+        director_data = db.session.query(Directors).filter(Directors.director_first_name == "Tony").first()
+        if actor_data == None:
+            print("Adding Tom Cruise...")
+            actor_first_name = "Tom"
+            actor_last_name = "Cruise"
+            new_actor = Actors(actor_first_name, actor_last_name)
+
+            db.session.add(new_actor)
+            db.session.commit()
+            
+        if director_data == None:
+            print("Adding Tony Scott...")
+            director_first_name = "Tony"
+            director_last_name = "Scott"
+            new_director = Directors(director_first_name, director_last_name)
+
+            db.session.add(new_director)
+            db.session.commit()
+            
+            print("Adding Top Gun...")
+            new_movie = Movies("Top Gun", new_actor.actor_id, actor_first_name, actor_last_name, new_director.director_id, director_first_name, director_last_name)
+            db.session.add(new_movie)
+            db.session.commit()
+            print("Movie Added! Starting Application...")
+
+    else:
+        print("Top Gun found! Starting Application!")
+        
 @app.route('/actors/add', methods=['POST'])
 def add_actor():
     form = request.form
